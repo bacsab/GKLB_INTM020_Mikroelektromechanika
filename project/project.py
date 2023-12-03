42121f97dc83b22cf5777225f5c20dfa0db671d8
bacsab <152679107+bacsab@users.noreply.github.com>
2023-12-03 12:12:57 +0100

    Update project.py
    Sensor and database

--- project/project.py
+++ project/project.py
@@ -5,3 +5,25 @@ import matplotlib.pyplot as plt #Grafikon megjelenítéséhez szükséges
 from datetime import datetime #Dátum és idő kezeléséhez szükséges
 
 import time #Időzítés használatához szükséges
+
+# Szenzor típusa és csatlakozás
+sensor = Adafruit_DHT.DHT22
+pin = 4  # Példa: 4-es GPIO tüske
+
+
+while True:
+    # Adatbázis csatlakozás
+    conn = sqlite3.connect('szenzor_adatok.db')
+    cursor = conn.cursor()
+
+    # Ellenőrizze, hogy a tábla már létezik-e, és csak akkor hozza létre, ha még nem létezik
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='meresek'")
+    if cursor.fetchone() is None:
+        cursor.execute('''
+            CREATE TABLE meresek (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                datum TIMESTAMP,
+                homerseklet REAL,
+                paratartalom REAL
+            )
+        ''')
