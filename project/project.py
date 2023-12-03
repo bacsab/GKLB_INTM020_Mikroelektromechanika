adc31c3031ee3877c7fe1c0ac0833ca1c7610a57
bacsab <152679107+bacsab@users.noreply.github.com>
2023-12-03 12:12:20 +0100

    Update project.py
    imports

--- project/project.py
+++ project/project.py
@@ -1,1 +1,7 @@
+import Adafruit_DHT #Szenzor felismeréséhez szükséges
+import sqlite3 #Adatbázis létrehozásához szükséges
+import csv #Adatbázis CSV-be exportálásához szükséges
+import matplotlib.pyplot as plt #Grafikon megjelenítéséhez szükséges
+from datetime import datetime #Dátum és idő kezeléséhez szükséges
 
+import time #Időzítés használatához szükséges
